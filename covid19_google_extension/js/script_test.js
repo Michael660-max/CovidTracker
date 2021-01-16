4c6163a23b24af86ec20188959f27b23b2505326
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-01-16 14:01:37 -0500

    Working data fetcher w/GitHub API

--- covid19_google_extension/js/script_test.js
+++ covid19_google_extension/js/script_test.js
@@ -1,5 +1,19 @@
+// This script gets data from GitHub
+// This script directly outputs variables onto the screen
+
 $.getJSON("https://covid-api.mmediagroup.fr/v1/cases", function(data){
 
-    console.log(data["Canada"]["Ontario"]);
+    var ontario_data = data["Canada"]["Ontario"];
+
+    document.getElementById("current_cases").innerHTML = ontario_data["confirmed"];
+    document.getElementById("recovered").innerHTML = ontario_data["recovered"];
+    document.getElementById("deaths").innerHTML = ontario_data["deaths"];
+
+    var recover_rate = 100 * ontario_data["recovered"] / (ontario_data["recovered"] + ontario_data["deaths"]);
+
+    document.getElementById("recovery_rate").innerHTML = recover_rate;
+
+    // Debug/Test
+    console.log(ontario_data);
 
 });
